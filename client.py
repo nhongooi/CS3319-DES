3cbfe2e14ca096779ac193a2db3c4ce5144e04e6
Tim Nguyen <tug56656@temple.edu>
2018-09-05 21:26:58 -0400

    init

---
+++ client.py
@@ -0,0 +1,12 @@
+## front end user interface
+
+class client(self):
+
+    def __init__(self):
+        pass
+
+    def connect_server(self):
+        pass
+
+    def chatt(self):
+        pass
---
+++ server.py
@@ -0,0 +1,13 @@
+### Back-end
+
+
+class server(self):
+
+    def __init__(self):
+        pass
+
+    def socket(self):
+        pass
+
+    def online(self):
+        pass
