09d2a9c8777aae6c596c56e3e01f64104faaf244
Tim Nguyen <nhongooi@gmai.com>
2018-09-07 09:39:19 -0400

    client/server with pyDes

--- client.py
+++ client.py
@@ -1,12 +1,59 @@
 ## front end user interface
+import socket
+import select
+import sys
+import pyDes
 
-class client(self):
 
-    def __init__(self):
-        pass
+class client:
 
-    def connect_server(self):
-        pass
+    def __init__(self, given_port, server_ip):
+        self.ip = server_ip
+        self.port = given_port
+        self.des_k = None
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def chatt(self):
-        pass
+    def chat(self):
+        """ Client's chat function"""
+        self.server.connect((self.ip, self.port))
+        self.get_key()
+        k = pyDes.des(self.des_k, pyDes.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
+        while True:
+            socket_list = [sys.stdin, self.server]
+            read_sockets, write_socket, err_socket = select.select(socket_list, [], [])
+
+            for sok in read_sockets:
+                if sok == self.server:
+                    msg = sok.recv(4096)
+                    #ciphertext
+                    print(msg)
+                    #message decrypt
+                    print(k.decrypt(msg))
+
+                else:
+                    msg = sys.stdin.readline()
+                    sys.stdout.write("[me]")
+                    sys.stdout.write(msg)
+                    # encrypt msg before sending
+                    self.server.send(k.encrypt(msg))
+                    sys.stdout.flush()
+
+        self.server.close()
+
+    def get_key(self):
+        socket_list = [sys.stdin, self.server]
+        read_sockets, write_socket, err_socket = select.select(socket_list, [], [])
+
+        for sok in read_sockets:
+            if sok == self.server:
+                self.des_k = (sok.recv(64)).encode()
+                print("key received")
+            else:
+                ConnectionAbortedError
+
+if __name__ == "__main__":
+    server = str(sys.argv[1])
+    port = int(sys.argv[2])
+
+    chat_client = client(port, server)
+    chat_client.chat()
--- server.py
+++ server.py
@@ -1,13 +1,82 @@
 ### Back-end
+import socket
+import select
+import random
+import sys
+import string
+import _thread
 
 
-class server(self):
+class server:
 
-    def __init__(self):
-        pass
+    def __init__(self, server_port, connection_num):
+        self.host_ip = socket.gethostbyname(socket.gethostname())
+        self.port = server_port
+        self.connect_num = connection_num
+        self.des_k = self.gen_key()
+        self.clients = []
 
-    def socket(self):
-        pass
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((self.host_ip, self.port))
+        self.server.listen(self.connect_num)
+
+    def clientThread(self, conn, addr):
+        """ Receive and broadcast client's msg"""
+        try:
+            # send each client connected the same key
+            conn.send(self.des_k)
+            conn.send("Welcome!")
+        except:
+            ConnectionAbortedError
+
+        while True:
+            try:
+                msg = conn.recv(4096)
+                if msg:
+                    broadcast_msg = "[" + addr[0] + "] " + msg
+                    print(broadcast_msg)
+                    self.broadcast(broadcast_msg, conn)
+                else:
+                    self.remove_conn(conn)
+            except:
+                continue
+
+    def broadcast(self, msg, conn):
+        """ Send msg to every client"""
+        for client in self.clients:
+            if client != conn:
+                try:
+                    client.send(msg)
+                except:
+                    client.close()
+
+    def remove_conn(self, conn):
+        """ remove connections from client list"""
+        if conn in self.clients:
+            self.clients.remove(conn)
 
     def online(self):
-        pass
+        """ Enable the server """
+        while True:
+            conn, addr = self.server.accept()
+            self.clients.append(conn)
+            print(addr[0], " connected")
+            _thread.start_new_thread(self.clientThread, (conn, addr))
+
+    def offline(self):
+        """ Turn off server"""
+        self.server.close()
+
+    def gen_key(self):
+        """ random generate 64 bytes key"""
+        return "".join([random.choice(string.ascii_letters) for x in range(8)])
+
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    conn_num = int(sys.argv[2])
+
+    chat_server = server(port, conn_num)
+    chat_server.online()
+    chat_server.offline()
